6c3ff75f87568f781f608841d16c351463b29ba8
Mateus <mateus.marques100@hotmail.com>
2024-04-01 20:28:10 -0300

    Ajuste do botao de salvar csv

--- src/main/java/com/example/dadosmeteorologicos/controller/LeitorCsvController.java
+++ src/main/java/com/example/dadosmeteorologicos/controller/LeitorCsvController.java
@@ -48,15 +48,15 @@ public class LeitorCsvController {
     void selecionarCsv(ActionEvent event) {
         // Obtém a referência do Stage atual
         Stage stage = (Stage) selecionarArquivo.getScene().getWindow();
-        salvarCsvButton.setVisible(true);
         FileChooser fileChooser = new FileChooser();
         fileChooser.setTitle("Selecionar arquivo CSV");
         fileChooser.getExtensionFilters().add(new FileChooser.ExtensionFilter("Arquivos CSV", "*.csv"));
         // Passa a referência do Stage para o método
         File CsvEntrada = fileChooser.showOpenDialog(stage); 
         caminhoArquivo = CsvEntrada.getAbsolutePath();
-        if (CsvEntrada != null) {
-            System.out.println("Arquivo selecionado: " + CsvEntrada.getAbsolutePath());
+        if (caminhoArquivo != null) {
+            System.out.println("Arquivo selecionado: " + caminhoArquivo);
+            salvarCsvButton.setVisible(true);
         }
     }
 
